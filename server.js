bedb79897f3a5620d1b424aa50a5f06a87e959fb
Hannah Nordgren <hey@hereshannahs.info>
2015-09-20 18:59:56 -0400

    refamiliarizing myself with node

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
-var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var port = process.env.port || 8080;
+var User = require('./app/models/user');
 
- mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+mongoose.connect('mongodb://localhost:27017/myDatabase');
 
- app.use(express.static(__dirname) + '/public');
- app.use(morgan('dev'));
- app.use(bodyParser.urlencoded({'extended': 'true'}));
- app.use(bodyParser.json());
- app.use(bodyParser.json({type: 'appication/vnd.ap+json'}));
- app.use(methodOverride());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
- app.listen(8080);
- console.log('the magic happens on 8080');
+app.use(function(req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+  next();
+});
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+  res.send('Welcome to the home page');
+});
+
+var apiRouter = express.Router();
+
+apiRouter.use(function(req, res, next){
+  console.log('hello app');
+  next();
+});
+
+apiRouter.get('/', function(req, res){
+  res.json({message: 'welcome to the API!'});
+});
+
+apiRouter.route('/users')
+  .post(function(req, res){
+    var user = new User();
+    user.name = req.body.username;
+    user.password = req.body.password;
+  })
+
+
+app.use('/api', apiRouter);
+
+app.listen(port);
+console.log('Get on the ' + port + ' level.');
